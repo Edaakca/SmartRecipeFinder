4a2abb30e31e5da9bdd9fbac425a1c5642e82050
Eda Akça <ednr6046@gmail.com>
2026-02-09 23:07:39 +0300

    Türkçe karakter çevirisi yapıldı.

--- app/src/main/java/com/example/smartrecipefinder/Util/StringUtils.kt
+++ app/src/main/java/com/example/smartrecipefinder/Util/StringUtils.kt
@@ -1,8 +1,11 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.smartrecipefinder.Util
 
 
 import java.text.Normalizer
 import java.util.Locale
+
 
 fun String.toSearchable(): String {
     return Normalizer.normalize(this.lowercase(Locale("tr", "TR")), Normalizer.Form.NFD)
